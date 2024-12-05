89cac9391330c321bb8fd254f01fb2f3e552e3d7
illia <ichorny.ilya25@gmail.com>
2024-12-05 14:00:58 +0100

    add category class

--- src/main/java/com/ecommerce/ecommerce/Catalog/core/domain/category/Category.java
+++ src/main/java/com/ecommerce/ecommerce/Catalog/core/domain/category/Category.java
@@ -1,4 +1,23 @@
 package com.ecommerce.ecommerce.Catalog.core.domain.category;
 
+import com.ecommerce.ecommerce.Catalog.core.domain.EntityId;
+
 public class Category {
+    private EntityId<String> id;
+    private String name;
+    public Category(EntityId<String> id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public EntityId<String> getId() {
+        return id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
