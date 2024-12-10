e6c797194f0fdd395a87b0b4dcca2cfa3ab36c24
illia <ichorny.ilya25@gmail.com>
2024-12-10 20:40:39 +0100

    add id by creating of order

--- src/main/java/com/ecommerce/ecommerce/Delivery/core/usecases/CreateDelivery.java
+++ src/main/java/com/ecommerce/ecommerce/Delivery/core/usecases/CreateDelivery.java
@@ -1,7 +1,10 @@
 package com.ecommerce.ecommerce.Delivery.core.usecases;
 
 import com.ecommerce.ecommerce.Delivery.core.domain.Delivery;
+import com.ecommerce.ecommerce.Delivery.core.domain.DeliveryId;
 import com.ecommerce.ecommerce.Delivery.core.repositories.DeliveryRepository;
+
+import java.util.UUID;
 
 public class CreateDelivery {
     private final DeliveryRepository deliveryRepository;
@@ -10,6 +13,9 @@ public class CreateDelivery {
         this.deliveryRepository = deliveryRepository;
     }
     public Delivery execute(Delivery delivery) {
+        if(delivery.getId() == null) {
+            delivery.setId(new DeliveryId(UUID.randomUUID().toString()));
+        }
         return deliveryRepository.save(delivery);
     }
 }
